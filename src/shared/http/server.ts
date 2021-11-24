3381b590a1ced32e80e0c43f29940cc68cff3c19
César Augusto <cesarapolidorio@gmail.com>
2021-11-24 09:33:59 -0300

    Project error: UnhandledPromiseRejectionWarning: QueryFailedError: invalid input syntax for type uuid: 'products' (RESOLVIDO)

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import "express-async-errors";
 import cors from "cors";
 import { routes } from "./routes";
 import AppError from "@shared/errors/AppError";
+import { errors } from "celebrate";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use(errors());
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof AppError) {
         return res.status(error.statusCode).json({ status: "error", message: error.message });
